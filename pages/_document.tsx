a906b0a81b8dc8ecfaf08dbc0850818190876ccd
ComputelessComputer <jeeheontransformers@gmail.com>
2023-04-12 17:57:22 +0900

    ✅ FEAT: updated the description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,11 +4,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <meta name="description" content="John Jeong's personal website" />
+        <meta
+          name="description"
+          content="I am John Jeong, currently the Business Owner @ Team Normal."
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta charSet="utf-8" />
         <meta name="og:title" content="John Jeong" />
-        <meta name="og:description" content="John Jeong's personal website" />
+        <meta
+          name="og:description"
+          content="I am John Jeong, currently the Business Owner @ Team Normal."
+        />
         <meta name="og:image" content="/images/johnjeong_og_image.png" />
         <meta name="og:url" content="https://johnjeong.com" />
         <meta name="og:site_name" content="John Jeong" />
