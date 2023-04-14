ee36bc19b643929786799acf2d09b523d6381a32
ComputelessComputer <jeeheontransformers@gmail.com>
2023-04-14 11:44:36 +0900

    ✅ FEAT: fixed ga

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,50 @@
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { useRouter } from "next/router";
+import Script from "next/script";
+import { useEffect } from "react";
+import * as gtag from "../lib/gtag";
+
+const isProduction = process.env.NODE_ENV === "production";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  useEffect(() => {
+    if (!isProduction) {
+      return;
+    }
+
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    router.events.on("hashChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+      router.events.off("hashChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
+  if (!isProduction) {
+    return <Component {...pageProps} />;
+  }
+
   return (
     <>
-      <GoogleAnalytics />
+      <Script
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.TRACKING_ID}`}
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', ${gtag.TRACKING_ID},{
+            page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Component {...pageProps} />
     </>
   );
