167874fba95a231b1755c366f1f439366ee693b7
정지헌 <63365510+ComputelessComputer@users.noreply.github.com>
2024-10-08 15:30:49 +0900

    Update Footer.tsx

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
     <footer className="w-full p-4">
-      <p className="text-xs">Copyright 2023 John Jeong</p>
+      <p className="text-xs">Copyright 2024 John Jeong</p>
     </footer>
   );
 };
