4c0c68fd1f7f7c508752ff04a448c04a8bae6b3f
John Jeong <jeeheontransformers@gmail.com>
2025-01-10 01:35:09 +0900

    added ga

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,53 +1,53 @@
-import Header from '@/components/Header'
-import './globals.css'
+import Header from "@/components/Header";
+import "./globals.css";
 
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import Footer from '@/components/Footer'
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import { GoogleAnalytics } from "@next/third-parties/google";
+import Footer from "@/components/Footer";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  metadataBase: new URL('https://johnjeong.com'),
+  metadataBase: new URL("https://johnjeong.com"),
   title: {
-    default: 'John Jeong',
-    template: '%s · John Jeong',
+    default: "John Jeong",
+    template: "%s · John Jeong",
   },
   description: "It's time to build!",
   openGraph: {
-    title: 'John Jeong',
+    title: "John Jeong",
     description: "It's time to build!",
-    url: 'https://johnjeong.com',
-    siteName: 'John Jeong',
-    locale: 'en_US',
-    type: 'website',
+    url: "https://johnjeong.com",
+    siteName: "John Jeong",
+    locale: "en_US",
+    type: "website",
   },
   robots: {
     index: true,
     follow: true,
   },
   twitter: {
-    card: 'summary_large_image',
-    title: 'John Jeong',
+    card: "summary_large_image",
+    title: "John Jeong",
     description: "It's time to build!",
-    creator: '@computeless',
+    creator: "@computeless",
   },
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body className={`${inter.className} antialiased`}>
-        <Header/>
-       <div className='mx-auto container px-4 py-8'>
-        {children}
-        </div> 
-        <Footer/>
+        <GoogleAnalytics gaId={process.env.NEXT_PUBLIC_GA_TRACKING_ID!} />
+        <Header />
+        <div className="mx-auto container px-4 py-8">{children}</div>
+        <Footer />
       </body>
     </html>
-  )
+  );
 }
